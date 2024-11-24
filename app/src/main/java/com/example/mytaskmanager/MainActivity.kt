518ed9c5434ae2fbd58b0a2999d5f7bacd8cfcdf
Sala <Salambaye2001@gmail.com>
2024-11-24 22:45:24 +0100

    My task manager

--- app/src/main/java/com/example/mytaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/mytaskmanager/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         // Initialiser le RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewTasks)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = TaskAdapter(listOf("Acheter des courses", "Faire du sport", "Préparer la réunion"))
+        recyclerView.adapter = TaskAdapter(listOf("Acheter des courses", "Faire du sport", "Préparer la réunion", "Cuisiner"))
 
         // Toast de bienvenue
         Toast.makeText(this, "Bienvenue dans l'application de gestion de tâches", Toast.LENGTH_SHORT).show()
